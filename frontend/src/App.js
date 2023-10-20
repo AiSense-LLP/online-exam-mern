3997178d96c25f6fcc20e7832fd0a7681dec191f
Rahultota <rahultota2001@gmail.com>
2023-10-20 07:33:03 +0530

    add contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from "./components/Hero";
 import Login from "./components/login/login";
+import Contact from "./components/Contact"
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Hero />} />
         <Route path="/Login" exact element={<Login />} />
+        <Route path="/Contact" exact element={<Contact />} />
       </Routes>
     </>
   );
---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,87 @@
+import girl from '../images/contact-1.JPG';
+import contact from '../images/contact-2.JPG';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../css/Contact.css'; 
+
+export default function Contact() {
+  return (
+    <div className='bg-body-tertiary container-fluid'>
+      <Row>
+        <Col xs={12} sm={6} className="image-col">
+          <div className="bg-body-tertiary">
+            <img src={girl} alt="" className="img-fluid" />
+          </div>
+        </Col>
+        <Col xs={12} sm={6} className="form-col justify-content-center">
+          <div className="isolate bg-body-tertiary px-6 py-24 sm:py-32 lg:px-8">
+            <div className="mx-auto max-w-2xl text-center">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
+              <p className="mt-2 text-lg leading-8 text-gray-600">
+                Some contact information on how to reach out
+              </p>
+            </div>
+            <form  className="mx-auto mt-16 max-w-xl sm:mt-20 ">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                <div className="sm:col-span-2">
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="company"
+                      id="company"
+                      placeholder='Company'
+                      autoComplete="organization"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div className="sm:col-span-2">
+                
+                  <div className="mt-2.5">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder='Email'
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  
+                  <div className="mt-2.5">
+                    <textarea
+                      name="message"
+                      id="message"
+                      placeholder='Massage'
+                      rows={4}
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      defaultValue={''}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-10">
+                <button
+                  type="submit"
+                  className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </Col>
+        <Col xs={12} sm={6} className="image-col">
+          <div className="bg-body-tertiary">
+            <img src={contact} alt="" className="img-fluid" />
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+
