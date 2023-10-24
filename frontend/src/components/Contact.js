e191777fbf93d3e5a0d1d445e81a87af14f226ce
Rahultota <rahultota2001@gmail.com>
2023-10-24 07:58:55 +0530

    Use formik Contact Us page

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -3,8 +3,39 @@ import contact from '../images/contact-2.JPG';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../css/Contact.css'; 
+import { useFormik } from 'formik';
+
+
+const validate = values => {
+  const errors = {};
+  if (!values.company) {
+    errors.company = 'Required';
+  } else if (values.company.length > 15) {
+    errors.company = 'Must be 15 characters or less';
+  }
+
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
 
 export default function Contact() {
+
+  const formik = useFormik({
+    initialValues: {
+      company: '',
+      email: '',
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
   return (
     <div className='bg-body-tertiary container-fluid'>
       <Row>
@@ -21,8 +52,9 @@ export default function Contact() {
                 Some contact information on how to reach out
               </p>
             </div>
-            <form  className="mx-auto mt-16 max-w-xl sm:mt-20 ">
+            <form  className="mx-auto mt-16 max-w-xl sm:mt-20 " onSubmit={formik.handleSubmit}>
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              
                 <div className="sm:col-span-2">
                   <div className="mt-2.5">
                     <input
@@ -32,7 +64,11 @@ export default function Contact() {
                       placeholder='Company'
                       autoComplete="organization"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      onChange={formik.handleChange}
+             value={formik.values.company}
+
                     />
+                    {formik.errors.company ? <div>{formik.errors.company}</div> : null}
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -45,7 +81,10 @@ export default function Contact() {
                       placeholder='Email'
                       autoComplete="email"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      onChange={formik.handleChange}
+         value={formik.values.email}
                     />
+                     {formik.errors.email ? <div>{formik.errors.email}</div> : null}
                   </div>
                 </div>
 
